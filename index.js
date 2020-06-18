f3d1a7d760b743fd3423b7cf18f37eecf8b1a380
dglass-cellaware <daniel.glass@cellawaretech.com>
2020-06-18 16:51:30 -0500

    updated some shiz

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
 const sqlite3 = require('sqlite3');
 
+
+// Private var to store cached db path.
+var cachedDbPath = undefined;
+
+// Private function to initialize sqlite db instance based on dbPath var passed in (usage of cached vs non-cached).
+function initDb(dbPath) {
+    if (!dbPath) {
+        return new sqlite3.Database(cachedDbPath);
+    } else {
+        return new sqlite3.Database(dbPath);
+    }
+}
+
 module.exports = {
 
-    ResultStatus: {
-        OK: 0,
-        ERR: -1
-    },
-    executeQuery: function (dbPath, sql) {
 
-        var db = new sqlite3.Database(dbPath);
+    // Optional db path caching:
+    cacheDbPath(dbPath) {
+        cachedDbPath = dbPath;
+    },
+
+    // Execution of raw queries:
+    executeQuery: function (sql, dbPath) {
+
+        var db = initDb(dbPath);
 
         return new Promise((resolve, reject) => {
             console.log(`Executing SQL: ${sql}`);
             db.all(sql, (err, res) => {
                 if (err) {
-                    reject({sts: this.ResultStatus.ERR, data: err});
+                    reject(err);
                 } else {
-                    resolve({sts: this.ResultStatus.OK, data: res});
+                    resolve(res);
                 }
             });
         });
     },
-    execute: function (dbPath, sql) {
+    execute: function (sql, dbPath) {
 
-        var db = new sqlite3.Database(dbPath);
+        var db = initDb(dbPath);
 
         return new Promise((resolve, reject) => {
             console.log(`Executing SQL: ${sql}`);
             db.run(sql, (err) => {
                 if (err) {
-                    reject({sts: this.ResultStatus.ERR, data: err});
+                    reject(err);
                 } else {
-                    resolve({sts: this.ResultStatus.OK, data: {}});
+                    resolve({});
                 }
             });
         });
     },
-    executeBatch: function (dbPath, statements) {
+    executeBatch: function (statements, dbPath) {
 
-        var db = new sqlite3.Database(dbPath);
+        var db = initDb(dbPath);
 
         return new Promise((resolve, reject) => {
             db.serialize(function () {
@@ -50,48 +66,48 @@ module.exports = {
                     if (err) {
                         console.log(`Executing SQL: ROLLBACK`);
                         db.exec("ROLLBACK");
-                        reject({sts: this.ResultStatus.ERR, data: err});
+                        reject(err);
                     } else {
                         console.log(`Executing SQL: COMMIT`);
                         db.exec("COMMIT");
-                        resolve({ sts: this.ResultStatus.OK, data: {}});
+                        resolve({});
                     }
                 });
             });
         });
     },
-    executeInsert: function (dbPath, tableName, body) {
+
+    // Building of queries and subsequent execution:
+    executeInsert: function (tableName, body, dbPath) {
 
         var insertSql = this.buildInsert(tableName, body);
 
-        return this.execute(dbPath, insertSql);
+        return this.execute(insertSql, dbPath);
     },
-    executeDelete: function (dbPath, tableName, where) {
+    executeDelete: function (tableName, where, dbPath) {
         var deleteSql = this.buildDelete(tableName, where);
 
-        return this.execute(dbPath, deleteSql);
+        return this.execute(deleteSql, dbPath);
     },
-    executeUpdate: function (dbPath, tableName, set, where) {
+    executeUpdate: function (tableName, set, where, dbPath) {
 
         var updateSql = this.buildUpdate(tableName, set, where);
 
-        return this.execute(dbPath, updateSql);
+        return this.execute(updateSql, dbPath);
     },
-    executeSelect: function (dbPath, tableName, where) {
+    executeSelect: function (tableName, where, dbPath) {
 
         if (where !== undefined) {
 
             var selectSql = this.buildSelect(tableName, where);
 
-            return this.executeQuery(dbPath, selectSql);
+            return this.executeQuery(selectSql, dbPath);
         } else {
-            return this.executeQuery(dbPath, `select * from ${tableName}`);
+            return this.executeQuery(`select * from ${tableName}`, dbPath);
         }
-
-
-
-
     },
+
+    // Building of queries:
     buildInsert: function (tableName, body) {
         var insertSql = `insert into ${tableName} (`;
 
@@ -105,12 +121,12 @@ module.exports = {
 
         for (idx in values) {
             var val = values[idx];
-            if(!val) {
+            if (!val) {
                 insertSql += `null,`;
             } else {
                 insertSql += `'${val}',`;
             }
-            
+
         }
 
         insertSql = insertSql.substr(0, insertSql.lastIndexOf(',')) + ')';
@@ -121,7 +137,11 @@ module.exports = {
         var deleteSql = `delete from ${tableName} where `;
 
         for (var key in where) {
-            deleteSql += `${key} = '${where[key]}' and `;
+            if (where[key] === null) {
+                deleteSql += `${key} = null and `;
+            } else {
+                deleteSql += `${key} = '${where[key]}' and `;
+            }
         }
 
         deleteSql = deleteSql.substr(0, deleteSql.lastIndexOf(' and '));
@@ -132,13 +152,21 @@ module.exports = {
         var updateSql = `update ${tableName} set `;
 
         for (var key in set) {
-            updateSql += `${key} = '${set[key]}', `;
+            if (set[key] === null) {
+                updateSql += `${key} = null, `;
+            } else {
+                updateSql += `${key} = '${set[key]}', `;
+            }
         }
 
         updateSql = updateSql.substr(0, updateSql.lastIndexOf(', ')) + ' where ';
 
         for (var key in where) {
-            updateSql += `${key} = '${where[key]}' and `
+            if (where[key] === null) {
+                updateSql += `${key} = null and `
+            } else {
+                updateSql += `${key} = '${where[key]}' and `
+            }
         }
 
         updateSql = updateSql.substr(0, updateSql.lastIndexOf(' and '));
@@ -155,5 +183,16 @@ module.exports = {
         selectSql = selectSql.substr(0, selectSql.lastIndexOf(' and '));
 
         return selectSql;
+    },
+
+    // Other utility functions:
+    recordExists: async function (tableName, where, dbPath) {
+        try {
+            var res = await this.executeSelect(tableName, where, dbPath);
+            return res.length > 0;
+        } catch (e) {
+            return false;
+        }
+
     }
 };
